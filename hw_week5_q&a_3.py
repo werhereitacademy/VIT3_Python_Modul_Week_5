4c76d9f82140585d51cf9cc2caf6cba05b19ad64
Fatih <121514395+buyukakcali@users.noreply.github.com>
2024-02-22 13:36:15 +0100

    Part2: Answers of Q3

---
+++ hw_week5_q&a_3.py
@@ -0,0 +1,29 @@
+class Shape:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+
+class Rectangle(Shape):
+    def cal_area(self):
+        return self.width * self.height
+
+
+class Square(Shape):
+    def cal_area(self):
+        if self.width == self.height:
+            return self.width ** 2
+        else:
+            return 'This is not a square!'
+
+
+if __name__ == '__main__':
+    r1 = Rectangle(5, 6)
+    sq1 = Square(5, 6)
+    sq2 = Square(7,7)
+
+    print(f'The area of the rectangle named r1 is: {r1.cal_area()}')
+
+    print(f'The area of the square named sq1 is  : {sq1.cal_area()}')
+
+    print(f'The area of the square named sq2 is  : {sq2.cal_area()}')
