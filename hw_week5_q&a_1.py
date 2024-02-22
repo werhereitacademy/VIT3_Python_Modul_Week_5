58aff3598be3df49d086d4e930e01d26b7a8f602
Fatih <121514395+buyukakcali@users.noreply.github.com>
2024-02-22 13:35:46 +0100

    Part1: fixed variable name of Answers of Q1

--- hw_week5_q&a_1.py
+++ hw_week5_q&a_1.py
@@ -2,18 +2,19 @@ class Rectangle:
 
     def __init__(self, width, height):
         self.width = width
-        self.boy = height
+        self.height = height
 
     def perimeter(self):
-        return self.width * 2 + self.boy * 2
+        return self.width * 2 + self.height * 2
 
     def area(self):
-        return self.width * self.boy
+        return self.width * self.height
 
 
 if __name__ == '__main__':
     r1 = Rectangle(5, 7)
 
-    print(f'Perimeter of rectangle named r1 is: {r1.perimeter()}')
+    print(f'The perimeter of the rectangle named r1 is: {r1.perimeter()}')
 
-    print(f'Area of rectangle named r1 is: {r1.area()}')
+    print(f'The area of the rectangle named r1 is     : {r1.area()}')
+
